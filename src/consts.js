f4c6cdc3ee62fadc2b340bff7fe844a558317461
Client13 <76941677+Client13@users.noreply.github.com>
2022-09-19 18:14:51 +0530

    Create consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const COMETCHAT_CONSTANTS = {
-	APP_ID: 'APP_ID',
-	REGION: 'REGION',
-	AUTH_KEY: 'AUTH_KEY'
+	APP_ID: '2199006bdcbca06b',
+	REGION: 'us',
+	AUTH_KEY: '8d510f15de93cafde11a67f0cd4530ffbb383891'
 }
